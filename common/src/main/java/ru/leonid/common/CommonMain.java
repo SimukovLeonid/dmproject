4a3f3a4254c2d20932eb44b622a247a57e85bfe3
leonid <pcjmeil@gmail.com>
2023-03-06 23:11:08 +0200

    Feature_1 1. Создан мультимодульный проект Maven

---
+++ common/src/main/java/ru/leonid/common/CommonMain.java
@@ -0,0 +1,7 @@
+package ru.leonid.common;
+
+public class CommonMain {
+    public static void main(String[] args) {
+        System.out.println("TEST COMMON");
+    }
+}
---
+++ service/src/main/java/ru/leonid/service/ServiceMain.java
@@ -0,0 +1,7 @@
+package ru.leonid.service;
+
+public class ServiceMain {
+    public static void main(String[] args) {
+        System.out.println("TEST SERVICE");
+    }
+}
